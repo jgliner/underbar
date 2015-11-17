c08a436efbffab76baed2ce7cf805a4b42ce7847
Jarrett Gliner <jarrett.gliner@gmail.com>
2015-11-16 21:21:21 -0800

    Implement _.uniq

--- src/underbar.js
+++ src/underbar.js
@@ -98,6 +98,12 @@
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+    var arrTemp = [];
+    return _.filter(array, function(item) {
+      var arrTempIndex = _.indexOf(arrTemp, item) === -1;
+      arrTemp.push(item)
+      return arrTempIndex;
+    })
   };
 
 
