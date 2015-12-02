ec0b1d274cb76be7d5a81aedffb42cc203ad266f
Jarrett Gliner <jarrett.gliner@gmail.com>
2015-12-01 19:17:27 -0800

    Implement advanced _.invoke

--- src/underbar.js
+++ src/underbar.js
@@ -341,6 +341,9 @@
   // Calls the method named by functionOrKey on each value in the list.
   // Note: You will need to learn a bit about .apply to complete this.
   _.invoke = function(collection, functionOrKey, args) {
+    return _.map(collection, function(item) {
+      return typeof functionOrKey !== 'string' ? functionOrKey.apply(item, args) : String.prototype[functionOrKey].apply(item, args);
+    })
   };
 
   // Sort the object's values by a criterion produced by an iterator.
